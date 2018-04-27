c02f799853f46d8bbb14955694bae47db5275823
xiaopeng <12405301@qq.com>
2018-04-27 12:17:52 +0800

    login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,48 +1,78 @@
 import React, {Component} from 'react';
 import './index.css';
-import { List, InputItem, Toast } from 'antd-mobile';
+import {
+  InputItem, Button, Toast,
+  WingBlank, WhiteSpace, List,
+  Flex } from 'antd-mobile';
 
 class Login extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-        hasError: false,
-        value: '',
+      timeLeft: 5,
+      begin: 0
     };
   }
 
-    onErrorClick = () => {
-        if (this.state.hasError) {
-            Toast.info('Please enter 11 digits');
-        }
-    }
-    onChange = (value) => {
-        if (value.replace(/\s/g, '').length < 11) {
-            this.setState({
-                hasError: true,
-            });
+  beginCountDown = (event) => {
+    event.preventDefault();
+    console.log(this.state.timeLeft)
+    if (this.state.timeLeft > 0) {
+      this.setState({
+        begin: 1
+      })
+
+      let that = this
+      let interval = setInterval(function () {
+        console.log(that.state.timeLeft)
+        if (that.state.timeLeft == 0) {
+          clearInterval(interval);
+          that.setState({
+            begin: 0,
+            timeLeft: 5
+          })
         } else {
-            this.setState({
-                hasError: false,
-            });
+          that.setState({
+            timeLeft: that.state.timeLeft - 1
+          })
         }
-        this.setState({
-            value,
-        });
+      }, 1000)
     }
+  }
 
   render() {
     return (
-        <div className="root">
+      <div className="flex-container">
+        <div className="sub-title">手机号</div>
+
+        <WingBlank size="sm">
           <InputItem
-              type="phone"
-              placeholder="input your phone"
-              error={this.state.hasError}
-              onErrorClick={this.onErrorClick}
-              onChange={this.onChange}
-              value={this.state.value}
-          >手机号码</InputItem>
-        </div>
+            type="phone"
+            placeholder="请输入手机号"/>
+        </WingBlank>
+        <WhiteSpace size="lg" />
+
+        <div className="sub-title">验证码</div>
+
+        <WingBlank size="sm">
+          <div className="verify-container">
+            <InputItem
+              className="input-item"
+              type="number"
+              placeholder="请输入验证码"/>
+            <span className="verify-link" onClick={this.beginCountDown}>
+              { this.state.begin === 0 ? '获取验证码' : this.state.timeLeft}
+            </span>
+          </div>
+        </WingBlank>
+
+        <WhiteSpace size="lg" />
+        <WhiteSpace size="lg" />
+        <WhiteSpace size="lg" />
+
+        <Button type="primary">登录</Button>
+      </div>
     );
   }
 }
