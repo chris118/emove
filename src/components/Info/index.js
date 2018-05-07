39cde3bec01f0a8985a1789641aed5da6df1d6cc
xiaopeng <12405301@qq.com>
2018-05-07 21:04:01 +0800

    info page

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
-import { List, InputItem, Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import { List, InputItem, Button, WhiteSpace, WingBlank, Picker } from 'antd-mobile';
 
 import './index.css';
 
 const Item = List.Item;
+
+const seasons = [
+  {
+    label: '2013',
+    value: '2013',
+  },
+  {
+    label: '2014',
+    value: '2014',
+  },
+];
+
 class Info extends Component {
   next = (event) => {
     event.preventDefault();
@@ -16,6 +28,9 @@ class Info extends Component {
       <div className="info-container">
         <List renderHeader={() => '请在下方填写您的搬出地址'}>
           <InputItem placeholder="请在此填写您的搬出地址">搬出地址</InputItem>
+          <Picker data={seasons} cols={1} >
+            <List.Item arrow="horizontal">Single</List.Item>
+          </Picker>
           <Item extra="有" arrow="horizontal" onClick={() => {}}>有无电梯</Item>
           <Item extra="不需要" arrow="horizontal" onClick={() => {}}>需要拼装</Item>
           <InputItem placeholder="请填写搬运到车距离(单位米)">搬运距离</InputItem>
