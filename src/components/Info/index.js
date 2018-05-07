0099255b6511c64c53c1d5ebf5e6e579d1c65fd9
chris <12405301@qq.com>
2018-05-08 00:24:02 +0800

    picker view

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,22 +1,42 @@
 import React, {Component} from 'react';
 import { List, InputItem, Button, WhiteSpace, WingBlank, Picker } from 'antd-mobile';
+// import { createForm } from 'rc-form';
 
 import './index.css';
 
 const Item = List.Item;
 
-const seasons = [
+const elevators = [
   {
-    label: '2013',
-    value: '2013',
+    label: '有',
+    value: 1,
   },
   {
-    label: '2014',
-    value: '2014',
+    label: '无',
+    value: 0,
+  },
+];
+const assembles = [
+  {
+    label: '需要',
+    value: 1,
+  },
+  {
+    label: '不需要',
+    value: 0,
   },
 ];
 
 class Info extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      elevator: [1],
+      assemble: [0]
+    };
+  }
+
   next = (event) => {
     event.preventDefault();
 
@@ -24,15 +44,30 @@ class Info extends Component {
   }
 
   render() {
+    // const { getFieldProps } = this.props.form;
     return (
       <div className="info-container">
         <List renderHeader={() => '请在下方填写您的搬出地址'}>
           <InputItem placeholder="请在此填写您的搬出地址">搬出地址</InputItem>
-          <Picker data={seasons} cols={1} >
-            <List.Item arrow="horizontal">Single</List.Item>
+          <Picker
+            data={elevators}
+            cols={1}
+            value={this.state.elevator}
+            onChange={v => this.setState({ elevator: v })}
+            onOk={v => this.setState({ elevator: v })}
+          >
+            <List.Item arrow="horizontal">有无电梯</List.Item>
           </Picker>
-          <Item extra="有" arrow="horizontal" onClick={() => {}}>有无电梯</Item>
-          <Item extra="不需要" arrow="horizontal" onClick={() => {}}>需要拼装</Item>
+          <Picker
+            data={assembles}
+            cols={1}
+            value={this.state.assemble}
+            onChange={v => this.setState({ assemble: v })}
+            onOk={v => this.setState({ assemble: v })}
+          >
+            <List.Item arrow="horizontal">需要拼装</List.Item>
+          </Picker>
+
           <InputItem placeholder="请填写搬运到车距离(单位米)">搬运距离</InputItem>
         </List>
         <List renderHeader={() => '请在下方填写您的搬入地址'}>
@@ -53,3 +88,4 @@ class Info extends Component {
   }
 }
 export default Info;
+// export default createForm()(Info);
